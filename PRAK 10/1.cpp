cea4bf52afbdb7672250cef535865635d538d5c1
Muhammad didik wahyudi <72702021+didik20051397040@users.noreply.github.com>
2021-06-07 21:15:22 +0700

    Add files via upload

---
+++ PRAK 10/1.cpp
@@ -0,0 +1,26 @@
+//MUHAMMAD DIDIK WAHYUDI
+//20051397040
+//MANAJEMEN INFORMATIKA
+#include <conio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+for(int i=0;i<20;i++)
+
+cout<<"MUHAMMAD DIDIK WAHYUDI\n";
+cout<<"20051397040\n";
+cout<<"MANAJEMEN INFORMATIKA 2020B\n";
+cout<<"\nData yang ingin dicari : ";
+i=0;
+posisi=0;
+while(i<19 && A[i]!=c){
+ i++;
+}
+if (A[i]!=c){
+ cout<<"Maaf data yang dicari tidak ada";
+}else if(posisi=i+1)
+   cout<<"ditemukan pada posisi ke "<<posisi;
+getch();
+}
+
